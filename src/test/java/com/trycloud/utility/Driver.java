edd1a7b7fec4b2ce0fb307d0557832522111fbd1
Ateequllah Saraabi <ateequllah.saraabi@gmail.com>
2022-01-09 18:48:03 -0800

    remote-browser added

--- src/test/java/com/trycloud/utility/Driver.java
+++ src/test/java/com/trycloud/utility/Driver.java
@@ -34,7 +34,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "54.211.204.33";
+                        String gridAddress = "34.239.126.164";
                         URL url = new URL("http://"+ gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
